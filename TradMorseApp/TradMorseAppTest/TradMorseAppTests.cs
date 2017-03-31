a2bf3acf05b119bf0e0490c931afb694a8f24ded
Adrien LECELLIER <lecellier.adrien@gmail.com>
2017-03-31 15:40:04 +0200

    Commit numéro
    Il y a des erreurs

---
+++ TradMorseApp/TradMorseAppTest/TradMorseAppTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace testUnitMorseTraducteur
+{
+    [TestClass]
+    public class ProgramTests
+    {
+
+        [TestMethod]
+        public void testTraduireLettre()
+        {
+            string lettre = TradMorseApp.Program.Translate("a");
+
+            Assert.AreEqual(lettre, ".-");
+        }
+
+        [TestMethod]
+        public void testTraduireMot()
+        {
+            string mot = TradMorseApp.Program.Translate("bonjour");
+
+            Assert.AreEqual(mot, "-... --- -. .--- --- ..- .-.");
+        }
+
+
+
+
+
+
+    }
+}
